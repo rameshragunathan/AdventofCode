68e691178d92092b4576302291854cc7fe309a1b
Ramesh Ragunathan <35957731+raneshragunathan@users.noreply.github.com>
2018-02-13 17:36:31 +0000

    Add files via upload

--- Day2.py
+++ Day2.py
@@ -2,43 +2,49 @@ def file_get_contents(filename):
     with open(filename) as f:
         return [_.strip('\n') for _ in f.readlines()]
 
+
+
 def unwrap_present(present):
     dimensions = sorted([int(_) for _ in present.split("x")])
-    print(dimensions)
+  #  print(dimensions)
     return dimensions
+
 
 def get_present_footage(present):
     dimensions = unwrap_present(present)
-    footage = ((dimensions[0] * dimensions[1]) * 2)\
-    + ((dimensions[1] * dimensions[2]) * 2)\
-    + ((dimensions[0] * dimensions[2]) * 2)\
-    + (dimensions[0] * dimensions[1])
-    print (footage)
+    footage = ((dimensions[0] * dimensions[1]) * 2) \
+              + ((dimensions[1] * dimensions[2]) * 2) \
+              + ((dimensions[0] * dimensions[2]) * 2) \
+              + (dimensions[0] * dimensions[1])
+   # print (footage)
     return footage
+
 
 def get_required_paper(input):
     total_footage = 0
     for present in input:
-     total_footage += get_present_footage(present)
+        total_footage += get_present_footage(present)
 
-
-    print(total_footage)
+   # print(total_footage)
     return total_footage
 
-#def get_present_ribbon(present):
+
+def get_present_ribbon(present):
     dimensions = unwrap_present(present)
-    footage = (dimensions[0] * 2)\
-    + (dimensions[1] * 2)\
-    + (dimensions[0] * dimensions[1] * dimensions[2])
+    footage = (dimensions[0] * 2) \
+              + (dimensions[1] * 2) \
+              + (dimensions[0] * dimensions[1] * dimensions[2])
     return footage
 
-#def get_required_ribbon(input):
+
+def get_required_ribbon(input):
     total_footage = 0
     for present in input:
         total_footage += get_present_ribbon(present)
-        return total_footage
+
+    return total_footage
+
 
 inp = file_get_contents("input.txt")
-#print (inp)
 print get_required_paper(inp)
-#print get_required_ribbon(inp)
+print get_required_ribbon(inp)
